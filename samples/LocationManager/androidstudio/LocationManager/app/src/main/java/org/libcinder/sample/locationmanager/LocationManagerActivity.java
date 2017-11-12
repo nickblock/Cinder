804db8786225943efaf179d5734480253e56f152
Nick Block <nickblock@hotmail.com>
2017-11-12 11:56:41 +0000

    add android project using locationManager block

---
+++ samples/LocationManager/androidstudio/LocationManager/app/src/main/java/org/libcinder/sample/locationmanager/LocationManagerActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.sample.LocationManager;
+
+import org.libcinder.app.CinderNativeActivity;
+
+public class LocationManagerActivity extends CinderNativeActivity {
+    static final String TAG = "LocationManagerActivity";
+}
