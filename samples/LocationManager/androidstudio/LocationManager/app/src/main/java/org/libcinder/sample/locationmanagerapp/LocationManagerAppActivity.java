cc242afe1068cf84bf534383654025a70fd61136
Nick Block <nickblock@hotmail.com>
2017-11-12 13:49:40 +0000

    LocationManager for Android building, crash on startup

---
+++ samples/LocationManager/androidstudio/LocationManager/app/src/main/java/org/libcinder/sample/locationmanagerapp/LocationManagerAppActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.sample.locationmanagerapp;
+
+import org.libcinder.app.CinderNativeActivity;
+
+public class LocationManagerAppActivity extends CinderNativeActivity {
+    static final String TAG = "LocationManagerAppActivity";
+}
