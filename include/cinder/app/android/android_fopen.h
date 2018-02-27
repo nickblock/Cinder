f14f601b89dc1617d82fa16eb8449f80d5499de2
Nick Block <nickblock@hotmail.com>
2018-02-27 21:34:35 +0000

    override printf for android

--- include/cinder/app/android/android_fopen.h
+++ include/cinder/app/android/android_fopen.h
@@ -3,18 +3,24 @@
 
 #include <stdio.h>
 #include <android/asset_manager.h>
+#include <android/log.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
+
+
 /* hijack fopen and route it through the android asset system so that
    we can pull things out of our packagesk APK */
 
-void android_fopen_set_asset_manager(AAssetManager* manager);
+
 FILE* android_fopen(const char* fname, const char* mode);
 
 #define fopen(name, mode) android_fopen(name, mode)
+
+#define printf(...) __android_log_print(ANDROID_LOG_DEBUG, "LOG_D", __VA_ARGS__)
 
 #ifdef __cplusplus
 }
