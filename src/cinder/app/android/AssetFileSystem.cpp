31333b351e6aaac13618ff291225653ae34e9506
Nick Block <nickblock@hotmail.com>
2018-02-27 19:42:37 +0000

    files allowing c libs to use fopen on android assets

--- src/cinder/app/android/AssetFileSystem.cpp
+++ src/cinder/app/android/AssetFileSystem.cpp
@@ -91,4 +91,37 @@ long int AssetFileSystem_flength( AssetFileSystem_FILE * stream )
 	return n;
 }
 
+
+static int android_read(void* cookie, char* buf, int size) {
+  return AAsset_read((AAsset*)cookie, buf, size);
+}
+
+static int android_write(void* cookie, const char* buf, int size) {
+  return EACCES; // can't provide write access to the apk
+}
+
+static fpos_t android_seek(void* cookie, fpos_t offset, int whence) {
+  return AAsset_seek((AAsset*)cookie, offset, whence);
+}
+
+static int android_close(void* cookie) {
+  AAsset_close((AAsset*)cookie);
+  return 0;
+}
+
+#ifdef __cplusplus
+extern "C" {
+
+FILE* android_fopen(const char* fname, const char* mode) {
+
+  if(mode[0] == 'w') return NULL;
+
+	AAssetManager * assetMan = GetNativeAssetManager();
+  AAsset* asset = AAssetManager_open(assetMan, fname, 0);
+  if(!asset) return NULL;
+
+  return funopen(asset, android_read, android_write, android_seek, android_close);
+}
+}
+#endif
 } } } // namespace cinder::app
